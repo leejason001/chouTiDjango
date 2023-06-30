64b028ca15c9e38e3f7d6b29651ee2a921c04425
leejason <13131412952@163.com>
2023-06-30 10:33:31 +0800

    将 静态的chouTi的全部代码移植过来，并跑通

---
+++ registerAndLogin/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class userInfo(models.Model):
+    username = models.CharField(max_length=32, db_index=True)
+    email = models.EmailField(max_length=32, unique=True)
+    pwd = models.CharField(max_length=32)
+    ctime = models.DateTimeField(auto_now_add=True)
+
+class sendMsg(models.Model):
+    email = models.EmailField(max_length=32, unique=True)
+    code = models.CharField(max_length=6)
+    firstSendTime = models.DateTimeField()
+    timesLimit = models.IntegerField()
+
