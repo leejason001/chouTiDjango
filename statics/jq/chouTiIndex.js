662625e38baf7d96966dabd5d97ecfd91697a1ed
leejason <13131412952@163.com>
2023-07-03 18:42:28 +0800

    将js代码从html中分离出来

---
+++ statics/jq/chouTiIndex.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $(".tabContentItemPage").width($(".chouTiContent-middlePart").width() -$(".tabContentItem img").width())
+    $(".chouTiContentHead-middlePart li").click(function () {
+        me = $(this)
+        $(".chouTiContent-middlePart .tabContent").each(function (index, currentDom) {
+            if (!$(currentDom).hasClass("hideTabContent")) {
+                $(currentDom).addClass("hideTabContent")
+            }
+            if ($(currentDom).hasClass(me.attr("id"))) {
+                $(currentDom).removeClass("hideTabContent")
+            }
+        })
+
+    })
+    $(".register_title .closeButton").click(function () {
+        $(this).parents(".maskBackground").addClass("css_hide")
+    })
+    $(".identityContainer").click(function () {
+        $(".register_title .closeButton").parents(".maskBackground").removeClass("css_hide")
+    })
+})
